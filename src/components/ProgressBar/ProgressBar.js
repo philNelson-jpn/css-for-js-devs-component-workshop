fb1237864b70f69b032a1b5521d1dc4b43e34438
Phil Nelson <nelsonpa@miamioh.edu>
2022-03-02 15:24:49 +0900

    adding styling and padding to the large progress bar only using a STYLE object and an additional wrapper to maintain overflow hidden effects on large percentages

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,36 +5,64 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const STYLES = {
+  small: {
+    height: 8,
+    padding: 0,
+  },
+  medium: {
+    height: 12,
+    padding: 0,
+  },
+  large: {
+    height: 16,
+    padding: 4,
+  },
+
+}
+
 const ProgressBar = ({ value, size }) => {
 
-  const height = size === 'small' ? 8 : 12;
+  const styles = STYLES[size];
+
+  if(!styles){
+    throw new Error(`Unknown height passed to ProgressBar: ${size}`)
+  }
 
   return <Wrapper 
     role="progressbar" 
     aria-valuenow={value} 
     aria-valuemin="0" 
-    aria-valuemax="100">
+    aria-valuemax="100"
+    style={{'--padding': styles.padding + 'px'}}>
       <VisuallyHidden>{value}%</VisuallyHidden>
+      
 
-  <Bar style={{'--width': value + '%', '--height': height + 'px'}}/>
+  <BarWrapper>
+    <Bar style={{'--width': value + '%', '--height': styles.height + 'px'}}/>
+  </BarWrapper>
   </Wrapper>
   ;
 };
 
 const Wrapper = styled.div`
-  height: var(--height);
-  width: 100%;
+  padding: var(--padding);
+  border-radius: 4px;
   background: ${COLORS.transparentGray15};
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
-  border-radius: 4px;
-  /* Ensure that the progress bar fills its container fluidly, border radius and all */
-  overflow: hidden;
+`;
+
+const BarWrapper = styled.div`
+border-radius: 4px;
+// Ensure that the progress bar fills its container fluidly, border radius and all
+overflow: hidden;
 `;
 
 const Bar = styled.div`
   height: var(--height);
   width: var(--width);
   background: slateblue;
+  border-radius: 4px 0 0 4px;
 `;
 
 
