6023bdcbf0b31643d60bbaf00070d07ffeb8ed53
Phil Nelson <nelsonpa@miamioh.edu>
2022-03-03 09:37:56 +0900

    initial commit of custom dropdown using the native select tag and masking it with custom styles to preserve accessibility

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,39 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <SelectWrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <CustomDropdown>{value}
+      <Icon id="chevron-down" strokeWidth={1} size={24} />
+      </CustomDropdown>
+    </SelectWrapper>
   );
 };
+
+
+
+const SelectWrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+  opacity: 0;
+`;
+
+const CustomDropdown = styled.div`
+  color: ${COLORS.gray700};
+  font-size: ${16 / 16}rem;
+  border-radius: 8px;
+  padding: 12px 16px;
+  background: ${COLORS.transparentGray15};
+`;
 
 export default Select;
